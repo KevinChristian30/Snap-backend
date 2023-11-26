4c3824fa9b588b703ac5fc160692f318a6c76b94
Kevin <kdotchrist30@gmail.com>
2023-11-27 00:12:20 +0700

    initialized project

---
+++ src/test/java/id/kevinchristian/snap/SnapApplicationTests.java
@@ -0,0 +1,13 @@
+package id.kevinchristian.snap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
