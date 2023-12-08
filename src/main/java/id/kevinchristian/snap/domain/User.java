df29099c84c3b1571e3257a6b6ec455c361a95bf
Kevin Christian <kdotchrist30@gmail.com>
2023-12-08 15:04:52 +0700

    fix: remapped email to users and eager loaded roles

--- src/main/java/id/kevinchristian/snap/domain/User.java
+++ src/main/java/id/kevinchristian/snap/domain/User.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
-@Table(name = "users", indexes = {@Index(name = "uk_username", columnList = "username")})
+@Table(name = "users", indexes = {@Index(name = "uk_username", columnList = "email")})
 @Data
 public class User extends AbstractBaseEntity implements UserDetails {
     private static final long serialVersionUID = 3795327430537547105L;
@@ -33,7 +33,7 @@ public class User extends AbstractBaseEntity implements UserDetails {
     @Column(name = "last_name")
     private String lastName;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")}, inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
     private List<Role> roles;
 
