b43b5ce4c45c516ebd30842bf1142cf2e334272b
Kevin Christian <kdotchrist30@gmail.com>
2023-12-18 15:33:26 +0700

    chore: remove uneeded imports

--- src/main/java/id/kevinchristian/snap/dto/response/UserResponseDTO.java
+++ src/main/java/id/kevinchristian/snap/dto/response/UserResponseDTO.java
@@ -2,6 +2,10 @@ package id.kevinchristian.snap.dto.response;
 
 import java.util.List;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record UserResponseDTO(
     String email,
     List<String> roles,
