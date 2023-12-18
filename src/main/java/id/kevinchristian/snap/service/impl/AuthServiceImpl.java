f53ac426b4a2b47f03e728e59ab8080c0485062c
Kevin Christian <kdotchrist30@gmail.com>
2023-12-18 13:28:03 +0700

    chore: removed uneeded imports

--- src/main/java/id/kevinchristian/snap/service/impl/AuthServiceImpl.java
+++ src/main/java/id/kevinchristian/snap/service/impl/AuthServiceImpl.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
